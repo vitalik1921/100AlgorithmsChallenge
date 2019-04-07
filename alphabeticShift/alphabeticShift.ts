44d1aa21f8888565d05b080f3f17558d26d61532
Vitalii Shebela <vitalik.privat@gmail.com>
2019-04-07 18:56:35 +0200

    Solved alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,14 @@
 function alphabeticShift(inputString: string): string {
-
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return inputString
+        .split('')
+        .map((char) => {
+            const newIndex = alphabet.findIndex((item) => item === char) + 1;
+            return newIndex > (alphabet.length - 1)
+                ? alphabet[0]
+                : alphabet[newIndex];
+        })
+        .join();
 }
 
 console.log(alphabeticShift('crazy'));
