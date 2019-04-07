d7e8b243de73f625ddcc7be1a2d5dce980478985
Vitalii Shebela <vitalik.privat@gmail.com>
2019-04-07 20:43:25 +0200

    Solved alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,14 @@
 function alternatingSums(a: number[]): number[] {
-
+    const sums = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        const isEven = (i % 2) === 0;
+        if (isEven) {
+            sums[0] += a[i];
+        } else {
+            sums[1] += a[i];
+        }
+    }
+    return sums;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
