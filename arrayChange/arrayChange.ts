b6682d8a1e5b54541221b750253237de6c351640
Vitalii Shebela <vitalik.privat@gmail.com>
2019-04-08 10:19:30 +0200

    Solved arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,16 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let moves = 0;
+    let prevValue = inputArray[0];
+    
+    for (let i = 1; i < inputArray.length; i++) {
+        if (inputArray[i] <= inputArray[i-1]) {
+            moves += prevValue - inputArray[i] + 1;
+            prevValue += moves;
+        }
+    }
+
+    return moves;
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([3, 4, 2]));
