7966736bdb72f7d7228ba167b73896556273f741
Vitalii Shebela <vitalik.privat@gmail.com>
2019-04-09 23:31:08 +0200

    Solved arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,12 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+    let maxSum = 0;
+    for (let i = 0; i <= (inputArray.length - k); i++) {
+        const subSequent: number[] = inputArray.slice(i, i + k);
+        maxSum = Math.max(maxSum, subSequent.reduce((acc, cur) => acc + cur, 0))
+    }
+    return maxSum;
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 3));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6, 1, 1, 4, 5, 6, 3, 4], 3));
