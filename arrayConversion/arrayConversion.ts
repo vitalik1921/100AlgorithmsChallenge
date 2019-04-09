33ae5939b09cc8c8449e07cf546d0dabdc75db43
Vitalii Shebela <vitalik.privat@gmail.com>
2019-04-09 23:11:16 +0200

    Solved arrayConversion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,27 @@
 function arrayConversion(inputArray: number[]): number {
+    let tempArray: number[] = inputArray;
+    let action = 'sum';
 
+    const performAction = (arr: number[], action: string) => {
+        return arr.reduce((acc, cur, idx) => {
+            if (((idx + 1) % 2) > 0) {
+                acc.push(
+                    action === 'sum'
+                        ? arr[idx] + arr[idx + 1]
+                        : arr[idx] * arr[idx + 1]
+                    );
+            }
+            return acc;
+        }, []);
+    };
+
+    while(tempArray.length > 1) {
+        tempArray = performAction(tempArray, action);
+        action = action === 'sum' ? 'mul' : 'sum';
+    }
+
+    return tempArray[0];
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]));
