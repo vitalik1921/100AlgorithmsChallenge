a5bbbd51e2ca1d31cffaafc929e402851ed5f10f
Vitalii Shebela <vitalik.privat@gmail.com>
2019-04-18 18:35:41 +0200

    Implemented arrayMaximalAdjacentDifference and arrayPreviousLess

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,9 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+    let maxDiff = 0;
+    for (let i = 1; i < inputArray.length; i++) {
+        maxDiff = Math.max(maxDiff, Math.abs(inputArray[i] - inputArray[i-1]));
+    }
+    return maxDiff;
 }
 
-console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0, 3, 4, 9, 2, 14]));
--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,18 @@
 function arrayPreviousLess(items: number[]): number[] {
-
+    const res = [];
+    for (let i = items.length - 1; i >=0; i--) {
+        i;
+        for (let j = i; j >= 0; j--) {
+            j;
+            if (items[i] > items[j]) {
+                res.unshift(items[j]);
+                break;
+            } else if (j === 0) {
+                res.unshift(-1);
+            }
+        }
+    }
+    return res;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
