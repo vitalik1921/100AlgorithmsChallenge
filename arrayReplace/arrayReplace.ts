34f6d7791b82a9f553347f51de65f0162554a202
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-07 15:09:21 +0200

    Resolved arrayReplace and avoidObstacles

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,5 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    return inputArray.map((item) => item === elemToReplace ? substitutionElem : item);
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,11 @@
 function avoidObstacles(inputArray: number[]): number {
-
+    inputArray.sort((a, b) => a - b);
+    const largestJump = inputArray[inputArray.length - 1] + 1;
+    for (let jump = 1; jump <= largestJump; jump++) {
+        if (inputArray.every((item) => item % jump !== 0)) {
+            return jump;
+        }
+    }
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
