8ff81d4cabd2b7119a9decaba5d9b7cdcb0477e3
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-07 15:51:41 +0200

    Resolved boxBlur

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,19 @@
 function boxBlur(image: number[][]): number[][] {
+    const allValues = [...image[0], ...image[1], ...image[2]];
+    const pixelValue = Math.floor(
+        allValues.reduce((acc, cur) => acc + cur) / allValues.length
+    );
 
+    for (let n = 0; n < image.length; n++) {
+        const row = image[n];
+        for (let i = 0; i < row.length; i++) {
+            row[i] = pixelValue;
+        }
+    }
+
+    return image;
 }
 
-console.log(boxBlur([[1, 1, 1], 
-    [1, 7, 1], 
-    [1, 1, 1]]));
+console.log(boxBlur([[1, 1, 1],
+[1, 7, 1],
+[1, 1, 1]]));
