d76e0e1b51c692b99fc6dd567a8521d618279229
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-07 15:30:51 +0200

    Resolved bishopAndPawn

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,18 @@
 function bishopAndPawn(bishop: string, pawn: string): boolean {
+    const chessX = 'abcdefgh'.split('');
 
+    function getXIndex(crd) {
+        return chessX.findIndex((item) => item === crd[0]) + 1;
+    }
+
+    const bishopX = getXIndex(bishop);
+    const bishopY = parseInt(bishop[1]);
+    const pawnX = getXIndex(pawn);
+    const pawnY = parseInt(pawn[1]);
+
+    return Math.abs(bishopX - pawnX) === Math.abs(bishopY - pawnY);
 }
 
 console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn('e4', 'h1'));
+console.log(bishopAndPawn('h1', 'a8'));
