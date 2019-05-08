aa34b07195ff17a477127999cc6fb45a6a170ce4
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-08 16:29:50 +0200

    Resolved centuryFromYear

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,5 +1,7 @@
 function centuryFromYear(year: number): number {
- 
+    return year % 100 === 0
+        ? year / 100
+        : Math.floor(year / 100) + 1;
 }
 
 console.log(centuryFromYear(1905));
