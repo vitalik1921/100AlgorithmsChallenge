dc74a9a123db38991a5029ebdeca9be449391491
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-08 16:34:23 +0200

    Resolved checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,5 +1,6 @@
 function checkPalindrome(inputString: string): boolean {
-
+    const reversedString = inputString.split('').reverse().join('');
+    return inputString === reversedString;
 }
 
 console.log(checkPalindrome('aabaa'));
