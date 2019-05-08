4b214c92aad00d66aa4cabb6c8efb378cef9a68e
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-08 16:32:25 +0200

    Resolved characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,5 +1,10 @@
 function characterParity(symbol: string): string {
-   
+    const digit = parseInt(symbol, 10);
+    if (Number.isNaN(digit)) {
+        return 'not a digit';
+    } else {
+        return digit % 2 === 0 ? 'even' : 'odd';
+    }
 }
 
 console.log(characterParity('5'))
