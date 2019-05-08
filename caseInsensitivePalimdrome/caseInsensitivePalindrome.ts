86fa2e08e8e4268636470266b3e3c6ca6226741b
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-08 16:25:08 +0200

    Resolved isCaseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,7 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    const lowerString = inputString.toLocaleLowerCase();
+    const reversedString = lowerString.split('').reverse().join('');
+    return lowerString === reversedString;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
