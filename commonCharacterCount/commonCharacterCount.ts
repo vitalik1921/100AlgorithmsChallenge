192b11b6126ea4418eaa079150e848003bf2d217
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-09 14:22:19 +0200

    Resolved commonCharacterCount

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,24 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+    const s1Set = new Map();
+    s1.split('').forEach((item) => {
+        s1Set.set(
+            item,
+            s1Set.has(item)
+                ? s1Set.get(item) + 1
+                : 1
+        );
+    });
+    const commonLetters = s2.split('').filter((item) => {
+        const count = s1Set.get(item);
+        if (s1Set.has(item) && count > 0) {
+            s1Set.set(item, count - 1);
+            return true;
+        } else {
+            return false;
+        }
+    });
+    return commonLetters.length;
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount('qwertyyy', 'qwertyyyyghjjk'));
