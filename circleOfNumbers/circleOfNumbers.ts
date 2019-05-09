27acc0a93e46afb68e92963ffa07bcca0797198f
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-09 09:39:04 +0200

    Resolved circleOfNumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,19 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
-
+    const nHalf = n / 2;
+    if (firstNumber < nHalf) {
+        return firstNumber + nHalf;
+    } else {
+        return firstNumber - nHalf;
+    }
 }
 
+console.log(circleOfNumbers(10, 0));
+console.log(circleOfNumbers(10, 1));
 console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 3));
+console.log(circleOfNumbers(10, 4));
+console.log(circleOfNumbers(10, 5));
+console.log(circleOfNumbers(10, 6));
+console.log(circleOfNumbers(10, 7));
+console.log(circleOfNumbers(10, 8));
+console.log(circleOfNumbers(10, 9));
