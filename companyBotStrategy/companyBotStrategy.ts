9ae3c7bd1313820f4f9f243dc4ef3197ec9074c0
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-09 14:32:26 +0200

    Resolved companyBotStrategy

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,5 +1,13 @@
 function companyBotStrategy(trainingData: number[][]): number {
- 
+    let totalAnswersTime = 0;
+    let totalCorrectAnswers = 0;
+    for (let i = 0; i < trainingData.length; i++) {
+        if (trainingData[i][1] === 1) {
+            totalAnswersTime += trainingData[i][0];
+            totalCorrectAnswers++;
+        }
+    }
+    return totalAnswersTime / totalCorrectAnswers || 0;
 }
 
 console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
