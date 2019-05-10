5ea725fda7bd3d26b40c3188cb93cfe79d088afb
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-10 09:32:59 +0200

    Resolve confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,7 @@
 function confirmEnding(str: string, target: string) {
-   
+    return str.slice(str.length - target.length) === target;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
-console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Open sesame", "same"));
+console.log(confirmEnding("hello", "llo"));
