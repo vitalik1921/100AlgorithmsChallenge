c703c15bf75fb85a3e8b714fb71b56f54d841ef9
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-10 11:28:24 +0200

    Resolve containsCloseNums

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,5 +1,27 @@
 function containsCloseNums(nums: number[], k: number): boolean {
+    const numsMap = new Map();
 
+    for (let i = 0; i < nums.length; i++) {
+        if (numsMap.has(nums[i])) {
+            const val = numsMap.get(nums[i]);
+            val.push(i);
+            numsMap.set(nums[i], val);
+        } else {
+            numsMap.set(nums[i], [i]);
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (numsMap.has(nums[i])) {
+            const indxArr = numsMap.get(nums[i]);
+            for (let n of indxArr) {
+                if (i !== n && Math.abs(i - n) <= k) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
