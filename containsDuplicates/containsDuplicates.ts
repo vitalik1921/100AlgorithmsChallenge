ae5d02fa759c994b5c834e6632b8a7d72a80917a
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-10 11:37:44 +0200

    Resolved containsDuplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,11 @@
 function containsDuplicates(a: number[]): boolean {
-
+    a.sort((a, b) => a - b);
+    for (let n = 1; n < a.length; n++) {
+        if (a[n] === a[n-1]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
