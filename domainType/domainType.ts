c8d8049c541c4748fd99d745056dbb39946920ba
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-13 08:59:40 +0200

    Resolved domainsMap

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,16 @@
-function domainType(domains: string[]): string[] {
+const domainsMap = {
+    com: 'commercial',
+    org: 'organization',
+    net: 'network',
+    info: 'information',
+};
 
+function domainType(domains: string[]): string[] {
+    return domains.map((domain) => {
+        const parts = domain.split('.');
+        const topLevel = parts[parts.length - 1];
+        return domainsMap[topLevel];
+    });
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
