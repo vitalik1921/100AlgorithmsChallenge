45f6d55b3561ab9ed9d3a62f96157ee492bf1ae1
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-13 09:09:39 +0200

    Resolved electionWinners and encloseInBrackets

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,7 @@
 function electionsWinners(votes: number[], k: number): number {
-
+    const curWinner = Math.max(...votes);
+    return votes
+        .filter((item) => (item + k) > curWinner).length;
 }
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
