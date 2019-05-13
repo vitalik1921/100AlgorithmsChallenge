571dc51e19e4093c59aca14726bb45b7a62a4632
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-13 08:51:34 +0200

    Resolved digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,18 @@
 function digitDegree(n: number): number {
-  
+    let degree = 0;
+    let tempN = n;
+
+    while (tempN > 9) {
+        tempN = tempN
+            .toString()
+            .split('')
+            .reduce((acc, prev) => acc + parseInt(prev), 0);
+        degree++;
+    }
+
+    return degree;
 }
 
 console.log(digitDegree(5));
-console.log(digitDegree(10));
+console.log(digitDegree(100));
 console.log(digitDegree(91));
