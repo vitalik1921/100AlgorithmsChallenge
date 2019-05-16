377649ee0107c6e8694834fbafcb61bff45ec0d9
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-16 11:01:32 +0200

    Resolved fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,13 @@
 function fermactor(n: number): number[] {
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < i; j++) {
+            if (i ** 2 - j ** 2 === n) {
+                return [i, j];
+            }
+        }
+    } 
 
+    return null;
 }
 
 console.log(fermactor(15));
