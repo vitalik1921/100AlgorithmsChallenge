33fa2b2c1f1b86729d0a43a8b1d0e4eab7839873
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-16 11:24:42 +0200

    Resolved firstDigit

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,5 +1,14 @@
 function firstDigit(inputString: string): string {
+    const digits = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
 
+    for (let i = 0; i < inputString.length; i++) {
+        const curCar = inputString.charAt(i);
+        if (digits.has(curCar)) {
+            return curCar; 
+        }
+    }
+
+    return '';
 }
 
 console.log(firstDigit('var_1__Int'));
