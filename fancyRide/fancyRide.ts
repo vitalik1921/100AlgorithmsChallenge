3b6965625aabebe07335dcfc5e9efd3a56dc003f
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-16 10:39:23 +0200

    Resolved fancyRide

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,16 @@
 function fancyRide(l: number, fares: number[]): string {
+    const cars = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
+    let yourCar = null;
 
+    for (let i = 0; i < fares.length; i++) {
+        if (fares[i] * l < 20) {
+            yourCar = cars[i];
+        } else {
+            return yourCar;
+        }
+    }
+
+    return yourCar;
 }
 
-console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.4, 0.7, 1, 1.3]));
