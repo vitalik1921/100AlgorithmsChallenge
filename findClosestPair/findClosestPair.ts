f0083d74bd3625345da682030e3dc522bb862dcd
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-16 11:17:14 +0200

    Resolved findClosestPair

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,5 +1,29 @@
 function findClosestPair(numbers: number[], sum: number): number {
-   
+    const distances = [];
+    const nMap = new Map();
+    numbers.forEach((num, idx) => {
+        if (nMap.has(num)) {
+            const nArr = nMap.get(num);
+            nArr.push(idx);
+            nMap.set(num, nArr);
+
+        } else {
+            nMap.set(num, [idx]);
+        }
+    });
+    numbers.forEach((num, idx) => {
+        const target = sum - num;
+        if (nMap.has(target)) {
+            const targetIdxs = nMap.get(target);
+            targetIdxs
+                .filter((targetIdx) => targetIdx > idx)
+                .forEach((targetIdx) => {
+                    distances.push(Math.abs(idx - targetIdx));
+                });
+        }
+    });
+    distances.sort((a, b) => a - b);
+    return distances[0] || -1;
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
