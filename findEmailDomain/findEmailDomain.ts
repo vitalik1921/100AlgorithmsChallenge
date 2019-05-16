6a438dfec6b005c8074f1caf7f1e5907414dc602
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-16 11:20:31 +0200

    Resolved findEmailDomain - updated

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,5 +1,6 @@
 function findEmailDomain(address: string): string {
-
+    const lastIndex = address.lastIndexOf('@');
+    return address.slice(lastIndex + 1);
 }
 
 console.log(findEmailDomain('prettyandsimple@example.com'));
