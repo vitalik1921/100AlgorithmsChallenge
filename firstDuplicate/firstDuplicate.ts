06d488292c9d2ac2cf534ef8fd5dae12ee8f60b3
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-22 16:25:45 +0200

    Resolved firstDuplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,15 @@
 function firstDuplicate(a: number[]): number {
-    
+    const nSet = new Set();
+    const dIndexes = [];
+
+    for (let num of a) {
+        if (nSet.has(num)) {
+            return num;
+        }
+        nSet.add(num);
+    }
+
+    return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
