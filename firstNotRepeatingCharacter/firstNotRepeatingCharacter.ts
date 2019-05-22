6bc2394f61052081994a1e8202376ed4eb396170
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-22 17:09:43 +0200

    Resolved firstNotRepeatingCharacter

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,5 +1,21 @@
 function firstNotRepeatingCharacter(s: string): string {
+    const uMap = {};
 
+    for (let c of s) {
+        if (uMap.hasOwnProperty(c)) {
+            uMap[c] += 1;
+        } else {
+            uMap[c] = 1;
+        }
+    }
+
+    for (let [key, value] of Object.entries(uMap)) {
+        if (value === 1) {
+            return key;
+        }
+    }
+
+    return "_";
 }
 
 console.log(firstNotRepeatingCharacter('abacabad'));
