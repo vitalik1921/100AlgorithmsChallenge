a886ed1d352119261912d158eee17e42bd2f2afd
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-23 10:03:59 +0200

    Resolved incrementalBackups

--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,5 +1,13 @@
 function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
+    const res = new Set();
 
+    for (const c = 0; c < changes.length; c++) {
+        if (changes[c][0] > lastBackupTime) {
+            res.add(changes[c][1]);
+        }
+    }
+
+    return Array.from(res);
 }
 
 console.log(incrementalBackups(461620205, [[461620203, 1], 
