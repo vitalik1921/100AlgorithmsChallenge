448f201ee573555f896e28bdef9ffe4357fd2381
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-23 09:20:37 +0200

    Resolved growingPlant

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,20 @@
 function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    if (downSpeed >= upSpeed) {
+        return null;
+    }
 
+    let curHeight = 0;
+    let curDay = 0;
+
+    while(curHeight < desiredHeight) {
+        curHeight += upSpeed;
+        if (curHeight < desiredHeight) {
+            curHeight -= downSpeed;
+        }
+        curDay++;
+    }
+
+    return curDay;
 }
 
 console.log(growingPlant(100, 10, 910))
