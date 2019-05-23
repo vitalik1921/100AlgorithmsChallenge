df0eafacacaa299e335cb8f53d365c0c11872976
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-23 09:32:28 +0200

    Resolve houseOfCats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,5 +1,14 @@
 function houseOfCats(legs: number): number[] {
+    const possiblePeoples = legs / 2;
+    const res = [];
 
+    for(let p = 1; p <= possiblePeoples; p++) {
+        if ((legs - p*2) % 4 === 0) {
+            res.push(p);
+        }
+    }
+
+    return res;
 }
 
 console.log(houseOfCats(6));
