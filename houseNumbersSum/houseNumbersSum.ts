88c3da0cc8b073fafc77e3f44cf35de175181644
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-23 09:26:05 +0200

    Resolved houseNumbersSum

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,15 @@
 function houseNumbersSum(inputArray: number[]): number {
+    let sum = 0;
 
+    for (let n = 0; n < inputArray.length; n++) {
+        if (inputArray[n] === 0) {
+            return sum;
+        }
+
+        sum += inputArray[n];
+    }
+
+    return sum;
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
