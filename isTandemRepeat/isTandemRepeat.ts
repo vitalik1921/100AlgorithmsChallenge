e7b988890d36bc6486ec446956b36cfa0e29fe41
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-27 08:46:30 +0200

    Resolved isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,5 +1,13 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+    const wordLength = inputString.length;
+
+    if (wordLength % 2 === 0) {
+        if (inputString.slice(0, wordLength) === inputString.slice(-wordLength)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 console.log(isTandemRepeat('tandemtandem'))
