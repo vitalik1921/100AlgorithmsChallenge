6bb7a84d0c5fad5a4211fab5167f84f937bf9369
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-27 09:05:17 +0200

    Resoled lateRide

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,5 +1,10 @@
 function lateRide(n: number): number {
-
+    const hours = parseInt(String(n / 60));
+    const minuses = n % 60;
+    const time = hours.toString() + minuses.toString();
+    return time
+        .split('')
+        .reduce((acc, cur) => acc + parseInt(cur), 0);
 }
 
 console.log(lateRide(240));
