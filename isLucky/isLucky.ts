f67797394586bcae7fee9f0c39528391a9cbea11
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-27 08:41:44 +0200

    Resolved isLucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,19 @@
 function isLucky(n: number): boolean {
-    
+    const strNum = n.toString();
+    const midIndex = strNum.length / 2;
+
+    const firstHalf = strNum.slice(0, midIndex);
+    const secondHalf = strNum.slice(-midIndex);
+
+    const firstSum = firstHalf
+        .split('')
+        .reduce((acc, num) => acc + parseInt(num), 0);
+
+    const secondSum = secondHalf
+        .split('')
+        .reduce((acc, num) => acc + parseInt(num), 0);
+
+    return firstSum === secondSum;
 }
 
 console.log(isLucky(1230));
