427fa7be53b9e74b905c9fa42f4a7ad2f47dceb8
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-27 08:56:49 +0200

    Resolved largestOfFour

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,5 +1,16 @@
 function largestOfFour(nums: number[][]): number[] {
+    const res = [];
 
+    for (let i = 0; i < nums.length; i++) {
+        const subArray = nums[i];
+        let maxValue = Number.MIN_SAFE_INTEGER;
+        for (let n = 0; n < subArray.length; n++) {
+            maxValue = Math.max(maxValue, subArray[n]);
+        }
+        res.push(maxValue);
+    }
+
+    return res;
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
