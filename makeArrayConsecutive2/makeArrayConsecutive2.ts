1743ed5b54d243da9492542acf946dc0d1b06e18
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-30 10:34:15 +0200

    Resolved makeArrayConsecutive2

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,7 @@
 function makeArrayConsecutive2(statues: number[]): number {
-
+    statues.sort((a, b) => a - b);
+    return statues[statues.length - 1] - statues[0] + 1 - statues.length;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([1, 2, 3, 4, 5, 7, 9]));
