dfa1e0b2dd17d47bbdf745b47146fa462ac5a0d1
Vitalii Shebela <vitalik.privat@gmail.com>
2019-05-30 10:41:17 +0200

    Resolved matrixElementsSum

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,5 +1,13 @@
 function matrixElementsSum(matrix: any[][]): number {
-
+    let res = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] > 0 && (i === 0 || matrix[i - 1][j] > 0)) {
+                res += matrix[i][j];
+            }
+        }
+    }
+    return res;
 }
 
 console.log(matrixElementsSum(
